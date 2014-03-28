05b6133aa892f9400ed48aafd3cc18f9e4616b84
Simson L. Garfinkel <simsong@acm.org>
2014-03-28 15:44:06 -0400

    more features for sqlite3

--- feature_sql.cpp
+++ feature_sql.cpp
@@ -2,18 +2,23 @@
  * Feature recorder that can write to an SQLite3 database
  */
 
-/* PRAGMAs Taken from auto_db.cpp in SleuthKit */
+/* http://blog.quibb.org/2010/08/fast-bulk-inserts-into-sqlite/ */
 
 #include "sqlite3.h"
 #include "stdio.h"
 #include "stdlib.h"
+#include "string.h"
 
 static const char *stmts[] = {
     "PRAGMA synchronous =  OFF;",
     "PRAGMA cache_size = 20000;",        // 10x normal cache
+    "PRAGMA journal_mode=MEMORY",
+    "PRAGMA temp_store=MEMORY",
     "CREATE TABLE db_info (schema_ver INTEGER, bulk_extractor_ver INTEGER);",
     "INSERT INTO db_info (schema_ver, bulk_extractor_ver) VALUES (1,1);",
     "CREATE TABLE features (offset INTEGER(12), path VARCHAR, feature TEXT, context TEXT)",
+    "CREATE INDEX features_idx ON features(offset);",
+    "CREATE INDEX features_vdx ON features(feature);",
     0};
 
 int main(int argc,char **argv)
@@ -33,4 +38,29 @@ int main(int argc,char **argv)
             exit(1);
         }
     }
+    /* Lets throw a million features into the table as a test */
+    sqlite3_exec(db,"BEGIN TRANSACTION",NULL,NULL,&errmsg);
+    const char *cmd = "INSERT INTO features VALUES (?1, ?2, ?3, ?4)";
+    sqlite3_stmt *stmt;
+    sqlite3_prepare_v2(db,cmd, strlen(cmd), &stmt, NULL);
+    for(int i=0;i<1000000;i++){
+        char buf[64];
+        snprintf(buf,sizeof(buf),"%d",i);
+        char feature[64];
+        snprintf(feature,sizeof(feature),"user%d@company.com",i);
+        char context[64];
+        snprintf(context,sizeof(context),"this is the context user%d@company.com yes it is!",i);
+        sqlite3_bind_int(stmt, 1, i);
+        sqlite3_bind_text(stmt, 2, buf, strlen(buf), SQLITE_STATIC);
+        sqlite3_bind_text(stmt, 3, feature, strlen(feature), SQLITE_STATIC);
+        sqlite3_bind_text(stmt, 4, context, strlen(context), SQLITE_STATIC);
+        if (sqlite3_step(stmt) != SQLITE_DONE) {
+            fprintf(stderr,"Commit Failed\n");
+        }
+        sqlite3_reset(stmt);
+        if(i%1000==0) printf("i=%d\n",i);
+    }
+    sqlite3_exec(db,"COMMIT TRANSACTION",NULL,NULL,&errmsg);
+    sqlite3_finalize(stmt);
+    sqlite3_close(db);
 }
