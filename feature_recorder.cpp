9d44cb0ee276de71811ac8b847022f8541b4810a
Simson Garfinkel <simsong@acm.org>
2014-04-18 01:12:46 -0400

    replaced hexval() with something a bit more reliable

--- feature_recorder.cpp
+++ feature_recorder.cpp
@@ -179,9 +179,24 @@ const std::string feature_recorder::bulk_extractor_version_header("# " PACKAGE_N
 
 static inline int hexval(char ch)
 {
-    if(ch>='0' && ch<='9') return ch-'0';
-    if(ch>='a' && ch<='f') return ch-'a'+10;
-    if(ch>='A' && ch<='F') return ch-'A'+10;
+    switch (ch) {
+    case '0': return 0;
+    case '1': return 1;
+    case '2': return 2;
+    case '3': return 3;
+    case '4': return 4;
+    case '5': return 5;
+    case '6': return 6;
+    case '7': return 7;
+    case '8': return 8;
+    case '9': return 9;
+    case 'a': case 'A': return 10;
+    case 'b': case 'B': return 11;
+    case 'c': case 'C': return 12;
+    case 'd': case 'D': return 13;
+    case 'e': case 'E': return 14;
+    case 'f': case 'F': return 15;
+    }
     return 0;
 }
 
