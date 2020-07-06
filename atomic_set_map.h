e8b0009c0cee9599deee76aa4a95dd0309f65b13
Simson Garfinkel <simsong@acm.org>
2020-07-06 19:38:10 -0400

    atomic histogram now works

--- atomic_set_map.h
+++ atomic_set_map.h
@@ -11,21 +11,20 @@
 
 #include <algorithm>
 #include <set>
-#include <map>
 #include <mutex>
 #include <unordered_map>
 #include <unordered_set>
 
 template <class TYPE,class CTYPE> class atomic_histogram {
     typedef std::unordered_map<TYPE,CTYPE> hmap_t;
-    hmap_t  amap; // the locked atomic map
+    hmap_t  amap;                         // the locked atomic map
     std::mutex M;                         // my lock
 public:
     atomic_histogram():amap(),M(){};
 
     // The callback is used to report the histogram.
     // The callback returns '0' if no error is encountered, '-1' if the dumping should stop
-    typedef int (*dump_callback_t)(void *user,const TYPE &val,const CTYPE &count);
+    typedef int (*dump_callback_t)(void *user, const TYPE &val, const CTYPE &count);
 
     // add and return the count
     // http://www.cplusplus.com/reference/unordered_map/unordered_map/insert/
@@ -40,7 +39,7 @@ public:
     }
 
     // Dump the database to a user-provided callback.
-    void dump(void *user,dump_callback_t dump_cb) const{
+    void dump(void *user,dump_callback_t dump_cb) const {
         const std::lock_guard<std::mutex> lock(M);
         for(typename hmap_t::const_iterator it = amap.begin();it!=amap.end();it++){
             int ret = (*dump_cb)(user,(*it).first,(*it).second);
@@ -61,19 +60,21 @@ public:
     };
     typedef std::vector< const ReportElement *> element_vector_t;
 
-    void     dump_sorted(void *user,dump_callback_t dump_cb) const {
+    // dump_sorted() can't be const because the callback might modify *user.
+    // (this took me a long time to figure out.)
+    //
+    void     dump_sorted(void *user,dump_callback_t dump_cb)  {
         /* Create a list of new elements, sort it, then report the sorted list */
         element_vector_t  evect;
         {
             const std::lock_guard<std::mutex> lock(M);
-            for(typename hmap_t::const_iterator it = amap.begin();it!=amap.end();it++){
-                evect.push_back( new ReportElement((*it).first, (*it).second));
+            for(const auto &it: amap){
+                evect.push_back( new ReportElement(it.first, it.second));
             }
         }
-        std::sort(evect.begin(),evect.end(),ReportElement::compare);
-        for(typename element_vector_t::const_iterator it = evect.begin();it!=evect.end();it++){
-            int ret = (*dump_cb)(user,(*it)->value,(*it)->tally);
-            delete *it;
+        std::sort(evect.begin(), evect.end(), ReportElement::compare);
+        for(const auto &it:evect){
+            int ret = (*dump_cb)(user,it->value,it->tally);
             if(ret<0) break;
         }
 
--- test_be13api_catch2.cpp
+++ test_be13api_catch2.cpp
@@ -25,3 +25,35 @@ TEST_CASE( "test atomic_set_map", "[vector]" ){
     REQUIRE( as.contains("one") == true );
     REQUIRE( as.contains("four") == false );
 }
+
+int dump_cb(void *user, const std::string &val, const int &count){
+    int *called = (int *)user;
+    switch (*called) {
+    case 0:
+        REQUIRE( val == "bar" );
+        REQUIRE( count == 10 );
+        break;
+    case 1:
+        REQUIRE( val == "foo" );
+        REQUIRE( count == 6 );
+        break;
+    default:
+        REQUIRE( false );
+    }
+    (*called) += 1;
+    return 0;
+}
+
+TEST_CASE( "test atomic_histogram", "[vector]" ){
+    atomic_histogram<std::string, int> ahist;
+    ahist.add("foo", 1);
+    ahist.add("foo", 2);
+    ahist.add("foo", 3);
+    ahist.add("bar", 10);
+
+
+    /* Now make sure things were added in the right order, and the right counts */
+    int called = 0;
+    ahist.dump_sorted( &called, dump_cb );
+    REQUIRE( called == 2);
+}
