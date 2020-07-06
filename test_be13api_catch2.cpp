5b72aabe00f10a8bd8db923d9bcf6bf254be93ce
Simson L. Garfinkel <simsong@acm.org>
2020-07-06 13:45:48 -0400

    updated test case

--- test_be13api_catch2.cpp
+++ test_be13api_catch2.cpp
@@ -1,21 +1,7 @@
 // https://github.com/catchorg/Catch2/blob/master/docs/tutorial.md#top
 
 #define CATCH_CONFIG_MAIN
-
 #include "config.h"
-
-//#ifdef DFXML_GNUC_HAS_IGNORED_SHADOW_PRAGMA
-//#pragma GCC diagnostic ignored "-Wshadow"
-//#endif
-
-//#ifdef DFXML_GNUC_HAS_IGNORED_UNUSED_VARIABLE_PRAGMA
-//#pragma GCC diagnostic ignored "-Wunused-variable"
-//#endif
-
-//#ifdef DFXML_GNUC_HAS_IGNORED_UNUSED_LABEL_PRAGMA
-//#pragma GCC diagnostic ignored "-Wunused-label"
-//#endif
-
 
 // define stuff I need in the global environment. Only read it once.
 #include "regex_vector.h"
