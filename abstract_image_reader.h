60b0da010e05f5869a500583db41958203a3c759
Simson Garfinkel <simsong@acm.org>
2021-08-19 23:04:29 -0400

    created an actual virtual function for ~abstract_image_reader

--- abstract_image_reader.h
+++ abstract_image_reader.h
@@ -4,10 +4,10 @@
 class abstract_image_reader {
 public:
     abstract_image_reader() {};
-    virtual ~abstract_image_reader() {};
+    virtual ~abstract_image_reader();
     virtual ssize_t pread(void *buf, size_t bufsize, uint64_t offset) const = 0;
     virtual int64_t image_size() const=0;
-    virtual const std::string &image_fname() const = 0;
+    virtual const std::string image_fname() const = 0;
 };
 
 #endif
