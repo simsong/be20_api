dee8002d84dc6fddaafaa68fadf56ebd96cbecd4
Simson Garfinkel <simsong@acm.org>
2021-09-11 09:59:27 -0400

    added another character counter

--- distinct_character_counter.h
+++ distinct_character_counter.h
@@ -1,20 +1,61 @@
 #ifndef DISTINCT_CHARACTER_COUNTER_H
 #define DISTINCT_CHARACTER_COUNTER_H
 
-class distinct_character_counter {
+class character_counter {
+public:;
+    class underflow : public std::exception {
+        const char *error {nullptr};
+    public:
+        underflow(const char *error_):error(error_) {}
+        const char* what() const noexcept override { return error; }
+    };
+};
+
+
+class highbit_character_counter : public character_counter {
+public:
+    highbit_character_counter(){};
+    int highbit_count {0};
+
+    void preload(const uint8_t *buf, size_t ct) {
+        for (size_t i = 0; i<ct; i++){
+            add(buf[i]);
+        }
+    }
+
+    void add(uint8_t ch) {
+        if (ch & 0x80){
+            highbit_count += 1;
+        }
+
+    }
+
+    void remove(uint8_t ch) {
+        if (ch & 0x80) {
+            if (highbit_count==0){
+                throw underflow("highbit_character_counter: underflow");
+            }
+            highbit_count -= 1;
+        }
+    }
+};
+
+
+class distinct_character_counter : public character_counter {
     size_t count[256];             // how many times we have seen this character
 public:
     distinct_character_counter() {
         memset(count,0,sizeof(count));
     };
 
-    class underflow : public std::exception {
-    public:
-        underflow() {}
-        const char* what() const noexcept override { return "distinct_character_counter::underflow"; }
-    };
-
     int distinct_count {0};
+
+    void preload(const uint8_t *buf, size_t ct) {
+        for (size_t i = 0; i<ct; i++){
+            add(buf[i]);
+        }
+    }
+
     void add(uint8_t ch) {
         count[ch] += 1;
         if (count[ch]==1) {
@@ -23,7 +64,9 @@ public:
     }
 
     void remove(uint8_t ch) {
-        if (count[ch]==0) throw underflow();
+        if (count[ch]==0){
+            throw underflow("distinct_character_counter: underflow");
+        }
         count[ch] -= 1;
         if (count[ch]==0) {
             distinct_count -= 1;
