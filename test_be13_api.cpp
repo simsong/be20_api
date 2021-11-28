dbba2654221d597ce90d2226f43b2fa100830e0f
Simson L. Garfinkel <simsong@acm.org>
2021-11-28 18:00:49 -0500

    added checks for path_printer

--- test_be13_api.cpp
+++ test_be13_api.cpp
@@ -1131,6 +1131,16 @@ TEST_CASE("test_image_reader", "[path_printer]"){
     REQUIRE(p.pread(buf, 10, 1000) == 0);
 }
 
+std::string hexdump=
+    "0000: 0001 0203 0405 0607 0809 0a0b 0c0d 0e0f 1011 1213 1415 1617 1819 1a1b 1c1d 1e1f ................................\n"
+    "0020: 2021 2223 2425 2627 2829 2a2b 2c2d 2e2f 3031 3233 3435 3637 3839 3a3b 3c3d 3e3f  !\"#$%&'()*+,-./0123456789:;<=>?\n"
+    "0040: 4041 4243 4445 4647 4849 4a4b 4c4d 4e4f 5051 5253 5455 5657 5859 5a5b 5c5d 5e5f @ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_\n"
+    "0060: 6061 6263 6465 6667 6869 6a6b 6c6d 6e6f 7071 7273 7475 7677 7879 7a7b 7c7d 7e7f `abcdefghijklmnopqrstuvwxyz{|}~.\n"
+    "0080: 8081 8283 8485 8687 8889 8a8b 8c8d 8e8f 9091 9293 9495 9697 9899 9a9b 9c9d 9e9f ................................\n"
+    "00a0: a0a1 a2a3 a4a5 a6a7 a8a9 aaab acad aeaf b0b1 b2b3 b4b5 b6b7 b8b9 babb bcbd bebf ................................\n"
+    "00c0: c0c1 c2c3 c4c5 c6c7 c8c9 cacb cccd cecf d0d1 d2d3 d4d5 d6d7 d8d9 dadb dcdd dedf ................................\n"
+    "00e0: e0e1 e2e3 e4e5 e6e7 e8e9 eaeb eced eeef f0f1 f2f3 f4f5 f6f7 f8f9 fafb fcfd feff ................................\n";
+
 TEST_CASE("printer","[path_printer]"){
     scanner_config sc;
     sc.outdir = get_tempdir();
@@ -1145,6 +1155,11 @@ TEST_CASE("printer","[path_printer]"){
     pp.process_path( "0-PRINT/r" );
 
     REQUIRE( mem_stream.str().size() == std::string("256\r\n").size() + p->image_size());
+
+    // Now try the other variants
+    mem_stream.str("");
+    pp.process_path( "0-PRINT/h" );
+    REQUIRE( mem_stream.str() == hexdump );
     delete p;
 }
 
